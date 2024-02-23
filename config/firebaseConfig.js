02a660fd1ea376ac07ca432aeb33c37ddfd43fc4
mr.erol <mr.erol@outlook.com>
2024-02-23 20:48:27 +0300

    empty firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -4,7 +4,7 @@ import config from './dotenvSetup.js';
 try {
   config();
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || {} ))
   });
 } catch (error) {
   console.error('Firebase initialization error:', error);
